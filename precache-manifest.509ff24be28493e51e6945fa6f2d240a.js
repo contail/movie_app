54fa00003b4e2820772ae68739f59bc0f4ba500c
contail <qkrtkdwls2121@gmail.com>
2018-11-24 22:56:23 +0900

    Updates

---
+++ precache-manifest.509ff24be28493e51e6945fa6f2d240a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "f174fd1cc858869093e4",
+    "url": "/movie_app/static/js/main.f174fd1c.chunk.js"
+  },
+  {
+    "revision": "ab4ce5f00a0c80c1deac",
+    "url": "/movie_app/static/js/1.ab4ce5f0.chunk.js"
+  },
+  {
+    "revision": "f174fd1cc858869093e4",
+    "url": "/movie_app/static/css/main.445957ae.chunk.css"
+  },
+  {
+    "revision": "6ad378178f90b86e54228cef91dc6ab8",
+    "url": "/movie_app/index.html"
+  }
+];
