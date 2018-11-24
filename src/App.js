a252cd415eb07dcdfc9d434db30217dff8fdfc2f
contail <qkrtkdwls2121@gmail.com>
2018-11-24 22:59:04 +0900

    complete react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
 
 class App extends Component {
+
+  state = {}
+
+  componentDidMount(){
+    this._getMovies()
+  }
+
+  _renderMovie = () =>{
+    const movies = this.state.movies.map( movie => {
+      console.log(movie)
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres = {movie.genres}
+        synopsis = {movie.synopsis}/>
+    })
+
+    return movies;
+  }
+
+   _getMovies = async () =>{
+    const movies = await this._callApi();
+
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(data => data.json())
+    .then (json => json.data.movies)
+    .catch(err =>  console.log(err))
+  }
+
+  
+
   render() {
+    const {movies} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = {movies ? "App" : "App--loading"}>
+       {this.state.movies ? this._renderMovie() : "Loading"}
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import LinesEllipsis from 'react-lines-ellipsis'
+import './Movie.css';
+
+
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            
+            <div className="Movie__Columns">
+                <h1> {title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre= {genre} key= {index} />)}
+                </div>
+
+                <p className="Movie__Synopsis"> 
+                <LinesEllipsis
+                    text = {synopsis}
+                    maxLine='3'
+                    ellipsis="..."
+                    trimRight
+                    basedOn='letters'
+                    />
+                </p>
+            </div>      
+        </div>
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre} </span>
+    )
+}
+
+function MoviePoster({poster, alt}){
+    return (
+        <img src = {poster} alt={alt} title={alt} className="Movie__Poster"/>
+    )
+}
+
+Movie.PropTypes ={
+    title : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired,
+    geners : PropTypes.string.isRequired,
+    synopsis : PropTypes.string.isRequired
+}
+
+MoviePoster.PropTypes = {
+    poster : PropTypes.string.isRequired,
+    alt : PropTypes.string.isRequired
+}
+
+MovieGenre.PropTypes = {
+    genre : PropTypes.string.isRequired
+}
+
+export default Movie;
